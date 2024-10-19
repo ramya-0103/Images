ba4e0c1327126f7d4a24b562e375dff96812d9a5
ramya-0103 <ramyasri.010305@gmail.com>
2024-10-20 01:29:50 +0530

    text

---
+++ App.js
@@ -0,0 +1,30 @@
+// src/App.js
+import React, { useState } from 'react';
+import News from './components/News';
+import Search from './components/Search';
+import CategoryFilter from './components/CategoryFilter';
+import './App.css'; // Import the CSS file
+
+const App = () => {
+  const [category, setCategory] = useState('');
+  const [query, setQuery] = useState('');
+
+  const handleSearch = (searchQuery) => {
+    setQuery(searchQuery);  // Update the search query
+  };
+
+  const handleSelectCategory = (selectedCategory) => {
+    setCategory(selectedCategory);
+    setQuery(''); // Reset the search query when selecting a new category
+  };
+
+  return (
+    <div>
+      <Search onSearch={handleSearch} />
+      <CategoryFilter onSelectCategory={handleSelectCategory} />
+      <News category={category} query={query} />
+    </div>
+  );
+};
+
+export default App;
